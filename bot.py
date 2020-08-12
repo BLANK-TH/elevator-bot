604447d0d2019009157a587610ff806644a182c7
BLANK-TH <blankdev.th@gmail.com>
2020-08-11 21:50:06 -0400

    Revive Command

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ import minesweeperPy
 import typing
 
 client = commands.Bot(command_prefix='s!')
-df = "Elevator Server Bot Ver.17.45.210 Developed By: BLANK"
+df = "Elevator Server Bot Ver.17.45.211 Developed By: BLANK"
 game = cycle(["A Bot for the Elevator Discord Server!",'Developed By: BLANK','Use s!help to see my commands!',df.replace(" Developed By: BLANK","")])
 hc = 0x8681bb
 client.remove_command('help')
@@ -3322,6 +3322,13 @@ async def bearspray(ctx,user:discord.Member=None):
     embed = discord.Embed(description=msg,colour=hc)
     embed.set_footer(text=df)
     embed.set_image(url="https://i.imgur.com/YVuheFh.jpg")
+    await ctx.message.channel.send(embed=embed)
+
+@client.command()
+async def revive(ctx,user:discord.Member):
+    embed = discord.Embed(description="{} has revived {}!".format(ctx.message.author.mention,user.mention))
+    embed.set_footer(text=df)
+    embed.set_image(url="https://media.giphy.com/media/3o7TKSM3u36i6yG4CI/giphy.gif")
     await ctx.message.channel.send(embed=embed)
 
 client.run(BOT_TOKEN)
