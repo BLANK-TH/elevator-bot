d32d8b83839865c357a40cfddb239c4922a2af96
BLANK-TH <blankdev.th@gmail.com>
2020-08-21 17:39:34 -0400

    Fix invalid option

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ import minesweeperPy
 import typing
 
 client = commands.Bot(command_prefix='s!')
-df = "Elevator Server Bot Ver.17.49.254 Developed By: BLANK"
+df = "Elevator Server Bot Ver.17.49.255 Developed By: BLANK"
 game = cycle(["A Bot for the Elevator Discord Server!",'Developed By: BLANK','Use s!help to see my commands!',df.replace(" Developed By: BLANK","")])
 hc = 0x8681bb
 client.remove_command('help')
