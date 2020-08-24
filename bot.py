5f2af8c9a7c9ab77e69ba0bc26d856370c840f5f
BLANK-TH <blankdev.th@gmail.com>
2020-08-24 13:29:22 -0400

    Fix typo

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ import minesweeperPy
 import typing
 
 client = commands.Bot(command_prefix='s!')
-df = "Elevator Server Bot Ver.17.51.266 Developed By: BLANK"
+df = "Elevator Server Bot Ver.17.51.267 Developed By: BLANK"
 game = cycle(["A Bot for the Elevator Discord Server!",'Developed By: BLANK','Use s!help to see my commands!',df.replace(" Developed By: BLANK","")])
 hc = 0x8681bb
 client.remove_command('help')
@@ -68,7 +68,7 @@ async def change_game():
 
 @client.event
 async def on_message(message):
-    if "S!" == message.content.lower()[:2]:
+    if "S!" == message.content[:2]:
         message.content = "s!" + message.content[2:]
     for mention in message.mentions:
         if 699677108607123548 == mention.id:
